888b7c3d4f6bbaa280448e487149cdc7534acf6b
Rodrigo Padilha <151454973+pad1ryoshi@users.noreply.github.com>
2025-06-28 18:35:12 -0300

    Create exercicio6.cpp
    [6] EXERCÍCIOS DE CONCEITOS BÁSICOS
    Faça um programa que realize a média aritmética entre 5 números.
    
    Entradas:
    - Cinco números inteiros.
    
    Saída:
    - A média aritmética destes cinco números.
    
    Exemplo de entrada
    5 4 3 2 1
    
    Exemplo de saída
    3

---
+++ exercicios/conceitos-basicos/exercicio6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        int a, b, c, d, e, soma, mediaAritmetica;
+
+        cin >> a;
+        cin >> b;
+        cin >> c;
+        cin >> d;
+        cin >> e;
+
+        soma = a + b + c + d + e;
+        mediaAritmetica = soma / 5;
+
+        cout << mediaAritmetica << endl;
+
+        return 0;
+}
